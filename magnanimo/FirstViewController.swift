0e720f207f86df93bf4c0ccf24c34ba08748399e
Aditya Srinivasan <asrinivasan@cinchapi.com>
2019-09-11 03:35:14 +0100

    Initial Commit

---
+++ magnanimo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  magnanimo
+//
+//  Created by Aditya Srinivasan on 9/11/19.
+//  Copyright © 2019 Aditya Srinivasan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ magnanimo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  magnanimo
+//
+//  Created by Aditya Srinivasan on 9/11/19.
+//  Copyright © 2019 Aditya Srinivasan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
